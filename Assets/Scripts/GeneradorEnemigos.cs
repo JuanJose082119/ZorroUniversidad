2bfd1083c7e41be4be978b0e7bf2112966b1005c
JuanJose082119 <32719927+JuanJose082119@users.noreply.github.com>
2023-03-30 11:47:13 -0500

    Spawn
    Co-Authored-By: salsarosada <124476577+salsarosada@users.noreply.github.com>

---
+++ Assets/Scripts/GeneradorEnemigos.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorEnemigos : MonoBehaviour
+{
+    [SerializeField] private GameObject enemigoPrefab;
+    [SerializeField] private float enemigoIntervalo;
+
+    private void Start()
+    {
+        StartCoroutine(GenEnemigos(enemigoIntervalo, enemigoPrefab));
+    }
+
+    private IEnumerator GenEnemigos(float intervalo, GameObject enemigo)
+    {
+        yield return new WaitForSeconds(intervalo);
+        GameObject nuevoEnemigo = Instantiate(enemigo, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(GenEnemigos(intervalo, enemigo));
+    }
+}
