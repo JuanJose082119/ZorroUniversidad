8e0f9b303544999627ea4b6ed9d04200fa979153
JuanJose082119 <32719927+JuanJose082119@users.noreply.github.com>
2023-03-30 12:42:37 -0500

    Corregido Nombre Script
    Co-Authored-By: salsarosada <124476577+salsarosada@users.noreply.github.com>

--- Assets/Scripts/EnemigoDrop.cs
+++ Assets/Scripts/EnemigoDrop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemigo : MonoBehaviour
+public class EnemigoDrop : MonoBehaviour
 {
     public GameObject drop;
     private void OnTriggerEnter(Collider other)
